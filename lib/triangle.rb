15ed2056ca1ec5ea3387259b8b886ae64d8379ab
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-30 03:33:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,19 @@
-require 'pry'
+require_relative 'pry'
 
 class Triangle
   attr_reader :kind
 
   def initialize(x, y, z)
+    @kind = type(x, y, z)
+  end
+
+  def type(x, y, z)
     if x <= 0 || y <= 0 || z <= 0
       begin
         raise TriangleError
       rescue TriangleError
         message "Invalid triangle"
+        nil
       end
     elsif [x, y, z].uniq.length == 1
       @kind = :equilateral
