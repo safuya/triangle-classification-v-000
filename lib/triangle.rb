eb3d2b822e8e37a18d45608f8dc691040d9814c0
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-30 04:18:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,6 +17,10 @@ class Triangle
     end
   end
 
+  def no_size?(array)
+    array.any? { |num| num <= 0 }
+  end
+
   def inequality_broken?(array)
     array.max >= array.min + array.sort[-2]
   end
