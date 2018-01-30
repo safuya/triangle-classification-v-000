25ae97e759151a8a7daaec1f3bf587aab3c0998d
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-30 03:27:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_reader :kind
+
+  def initialize(x, y, z)
+    binding.pry
+    if x <= 0 || y <= 0 || z <= 0
+      binding.pry
+      raise TriangleError
+    elsif [x, y, z].uniq.length == 1
+      @kind = :equilateral
+    elsif [x, y, z].uniq.length == 2
+      @kind = :isosceles
+    else
+      @kind = :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
