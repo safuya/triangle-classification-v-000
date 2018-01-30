78f9adf710ab8352f9ea1de9c3602bd0a7550f94
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-30 03:50:29 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,11 +6,7 @@ class Triangle
   def initialize(x, y, z)
     binding.pry
     if x <= 0 || y <= 0 || z <= 0
-      begin
-        raise TriangleError
-      rescue TriangleError => e
-        e.message
-      end
+      raise Error
     elsif [x, y, z].uniq.length == 1
       @kind = :equilateral
     elsif [x, y, z].uniq.length == 2
