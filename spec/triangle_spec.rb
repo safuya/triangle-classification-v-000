e1519b11c16fb7053e04a39d1da93520a0155e98
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-30 04:01:07 +0000

    automatically backed up by learn

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -45,19 +45,19 @@ describe 'Triangle' do
   end
 
   it 'knows that triangles with negative sides are illegal' do
-    expect{Triangle.new(3, 4, -5).kind}.to raise_error(TriangleError)
+    expect{Triangle.new(3, 4, -5).kind}.to raise_error(Triangle::TriangleError)
   end
 
   it 'knows that triangles violating triangle inequality are illegal' do
-    expect{Triangle.new(1, 1, 3).kind}.to raise_error(TriangleError)
+    expect{Triangle.new(1, 1, 3).kind}.to raise_error(Triangle::TriangleError)
   end
 
   it 'knows that triangles violating triangle inequality are illegal 2' do
-    expect{Triangle.new(2, 4, 2).kind}.to raise_error(TriangleError)
+    expect{Triangle.new(2, 4, 2).kind}.to raise_error(Triangle::TriangleError)
   end
 
   it 'knows that triangles violating triangle inequality are illegal 3' do
-    expect{Triangle.new(7, 3, 2).kind}.to raise_error(TriangleError)
+    expect{Triangle.new(7, 3, 2).kind}.to raise_error(Triangle::TriangleError)
   end
 
 end
